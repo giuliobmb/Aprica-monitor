7721f7d5076791d199fe18d6b0e8db8dccb40162
giuliobmbb <74022973+giuliobmb@users.noreply.github.com>
2022-02-14 19:34:46 +0100

    Update aprica.py

--- aprica.py
+++ aprica.py
@@ -15,10 +15,10 @@ def monitor():
 
     while (int(r.status_code) == 200):
         r = requests.get('https://www.trenord.it/rest/cta/tariff?id=79')
-        if(r.json()['units'] != 0 and r.json()['units'] < 50):
-            hook.send("Aprica life back in stock\nSTOCK => " + str(r.json()['units']))
+
+        hook.send("Aprica life back in stock\nSTOCK => " + str(r.json()['units']))
     
-    sleep(3600)
+        sleep(1800)
     
     hook.send("Aprica monitor stopped")
 
